7266a7732e63e692e19be8468cb11b6a19d7ff17
Maxanenko <hotmac2004@gmail.com>
2025-03-04 21:35:32 +0400

    Update PaintingCard component and pagination styles

--- src/components/app-layout/main/painting-card/PaintingCard.tsx
+++ src/components/app-layout/main/painting-card/PaintingCard.tsx
@@ -18,49 +18,6 @@ export default function PaintingCard({ painting }: { painting: Painting }) {
     error: authorError,
   } = useGetAuthorByIdQuery(painting.authorId);
 
-  if (isLocationLoading || isAuthorLoading) {
-    return (
-      <div className={card_s.card}>
-        <PaintingImg
-          imageUrl={painting.imageUrl}
-          alt="alt"
-          classes={{ cardImage: card_s.card__image }}
-        />
-        <div className={card_s.card__info}>
-          <div className={card_s.card__infoHover}>
-            <h2 className="card__info-title">{painting.name}</h2>
-            <time dateTime={painting.created}>{painting.created}</time>
-          </div>
-          <div className={card_s.card__infoMain}>
-            <h2 className="card__info-title">Автор еще не загружен</h2>
-            <p>Локация еще не загружена</p>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  if (locationError || authorError) {
-    return (
-      <div className={card_s.card}>
-        <PaintingImg
-          imageUrl={painting.imageUrl}
-          alt="alt"
-          classes={{ cardImage: card_s.card__image }}
-        />
-        <div className={card_s.card__info}>
-          <div className={card_s.card__infoHover}>
-            <h2 className="card__info-title">Автор не загружен</h2>
-            <p>Локация не загружена</p>
-          </div>
-          <div className={card_s.card__infoMain}>
-            <h2 className="card__info-title">{painting.name}</h2>
-            <time dateTime="2024">{painting.created}</time>
-          </div>
-        </div>
-      </div>
-    );
-  }
   return (
     <div className={card_s.card}>
       <PaintingImg
@@ -70,12 +27,20 @@ export default function PaintingCard({ painting }: { painting: Painting }) {
       />
       <div className={card_s.card__info}>
         <div className={card_s.card__infoHover}>
-          <h2 className="card__info-title">{painting.name}</h2>
-          <time dateTime="2024">{painting.created}</time>
+          <h2 className="card__info-title">
+            {!isAuthorLoading && !authorError
+              ? author?.name
+              : 'Автор не загружен'}
+          </h2>
+          <p>
+            {!isLocationLoading && !locationError
+              ? location?.location
+              : 'Локация не загружена'}
+          </p>
         </div>
         <div className={card_s.card__infoMain}>
-          <h2 className="card__info-title">{author?.name}</h2>
-          <p>{location?.location}</p>
+          <h2 className="card__info-title">{painting.name}</h2>
+          <time dateTime="2024">{painting.created}</time>
         </div>
       </div>
     </div>
